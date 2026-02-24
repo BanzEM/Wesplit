3114cb1736517337a9824d8b80542227b8899a7b
bamazom <bamazomjhb024@student.wethinkcode.co.za>
2026-02-24 19:39:37 +0200

    Initial Commit

---
+++ Wesplit/WesplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WesplitApp.swift
+//  Wesplit
+//
+//  Created by Bandisile Mazomba on 2026/02/24.
+//
+
+import SwiftUI
+
+@main
+struct WesplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
