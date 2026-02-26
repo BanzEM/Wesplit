5d5eb148c76d6a8cea5e7ffeb6d2e9c3a6317f7f
BanzEM <bandisilemazomba123@gmail.com>
2026-02-26 13:12:47 +0200

    added a form with amount section and textfield for entering amount

--- Wesplit/ContentView.swift
+++ Wesplit/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var checkAmount = 0.00
+    @State private var tipPercentage = 10
+    @State private var numberOfPeople = 2
+    
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+       NavigationStack {
+            Form {
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ??  "ZAR" ))
+                        .keyboardType(.decimalPad)
+                }
+                
+                Section{
+                    Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "ZAR"))
+                }
+           }
+            .navigationTitle("WeSplit"  )
+            .navigationBarTitleDisplayMode( .inline )
+            
+       }
+        
     }
 }
 
